f002991193ad15f11a54f853b6b0819b5f9fc200
Zoe Batz <zoeb99@verizon.net>
2023-04-21 23:15:55 -0400

    update login/signup

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -17,7 +17,36 @@ import { useLogin } from "../hooks/useLogin"
   }
 
   return (
-    <div style={styles.Container}>
+    <div className='Auth-form-container'>
+      <form className="Auth-form" onSubmit={handleSubmit}>
+        <div className='Auth-form-content'>
+          <h3 className='Auth-form-title'>Sign In</h3>
+            <div className='form-group mt-3'>
+              <label>Email:</label>
+              <input className="form-control mt-1"
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className='form-group mt-3'>
+              <label>Password:</label>
+              <input className="form-control mt-1"
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button disabled = {isLoading} type="submit" className='btn btn-primary'>Login</button>
+            </div>
+          <p className="label">Don't have an account? <Link to ="/signup">Sign Up</Link></p>
+        </div>
+      </form>
+    </div>
+/*    <div style={styles.Container}>
       
       <h1 style = {styles.Header}>Login</h1>
       <div style={{ color: 'red' }}>{error}</div>
@@ -37,6 +66,7 @@ import { useLogin } from "../hooks/useLogin"
       </form>
       <p style = {styles.Label}>Don't have an account? <Link to ="/signup">Sign up</Link></p>
     </div>
+    */
   );
 }
 
--- frontend/src/pages/NewUserPage.js
+++ frontend/src/pages/NewUserPage.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useSignup } from "../hooks/useSignup"
-
-
 import {styles} from '../styles';
 import {Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const NewUserPage = () => {
   const [password, setPassword] = useState('');
@@ -16,25 +16,63 @@ const NewUserPage = () => {
     await signup(email, password)
   }
   return (
-    <div style={styles.Container}>
+    <div className='Auth-form-container'>
+      <form className="Auth-form" onSubmit={handleSubmit}>
+        <div className='Auth-form-content'>
+          <h3 className='Auth-form-title'>Create Account</h3>
+            <div className='form-group mt-3'>
+              <label>Email:</label>
+              <input className="form-control mt-1"
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className='form-group mt-3'>
+              <label>Password: (Must contain at least 8 characters)</label>
+              <input className="form-control mt-1"
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button disabled = {isLoading} type="submit" className='btn btn-primary'>Sign up</button>
+            </div>
+            <p className="label">Already have an account? <Link to ="/login">Login</Link></p>
+        </div>
+      </form>
+    </div>
+  /*  <div style={styles.Container}>
       
       <h1 style = {styles.Header}>Register with Organizr</h1>
       <div style={{ color: 'red' }}>{error}</div>
       <form className ='signup' onSubmit={handleSubmit} style = {styles.FormContainer}>
-      <input style = {styles.Input}
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input style = {styles.Input}
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <label>
+          Email:
+          <input className="input" 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      
+        <label>
+          Password:
+          <input className="input" 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        
         <button disabled = {isLoading}>Sign up</button>
       </form>
-      <p style = {styles.Label}>Already have an account? <Link to ="/login">Login</Link></p>
+      <p className="label">Already have an account? <Link to ="/login">Login</Link></p>
     </div>
+    */
   );
 }
 
